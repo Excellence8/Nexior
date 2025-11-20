fa97441007fbe7cd6310ce386a5c407bc555274a
Excellence8 <dgsjk3258@126.com>
2025-11-20 15:39:50 +0800

    fix: 修复vite.config.ts语法错误

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
-﻿// cache-bust: 1
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
@@ -8,53 +7,4 @@ export default defineConfig({
   build: {
     outDir: 'dist'
   }
-})                                                                                                                                       import { type ConfigEnv, defineConfig, loadEnv } from "vite";
-import vue from "@vitejs/plugin-vue";
-import replace from "@rollup/plugin-replace";
-import * as path from "path";
-
-export default defineConfig((config: ConfigEnv) => {
-  process.env = { ...process.env, ...loadEnv(config.mode, process.cwd()) };
-  return {
-    server: {
-      host: "localhost",
-      port: 8084,
-      proxy: {
-        "/api/v1/auth": {
-          target: process.env.VITE_BASE_URL_AUTH,
-          changeOrigin: true
-        },
-        "/api/v1/users": {
-          target: process.env.VITE_BASE_URL_AUTH,
-          changeOrigin: true
-        },
-        "/oauth2/v1/token": {
-          target: process.env.VITE_BASE_URL_AUTH,
-          changeOrigin: true
-        },
-        "/api": {
-          target: process.env.VITE_BASE_URL_PLATFORM,
-          changeOrigin: true
-        },
-        "/static": {
-          target: process.env.VITE_BASE_URL_PLATFORM,
-          changeOrigin: true
-        }
-      }
-    },
-    plugins: [
-      vue(),
-      replace({
-        preventAssignment: true
-      })
-    ],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src")
-      }
-    },
-    optimizeDeps: {
-      include: ["json-logic-js", "psl", "uuid"]
-    }
-  };
-});
+})
